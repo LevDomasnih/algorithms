860f517b887e8cc8adf4553cdd113f243b10009f
Lev Domasnih <lev.domasnih@motus-group.ru>
2021-12-09 16:23:00 +0500

    Повтор алгоритма

--- binarySearch.js
+++ binarySearch.js
@@ -1,28 +1,22 @@
 const binarySearch = (list, item) => {
     let low = 0
     let high = list.length - 1
-    let times = 0 // not req
-    let timer = new Date().getMilliseconds()
 
     while (low <= high) {
-        times++ // not req
+        let middle = Math.floor((low + high) / 2)
+        let guess = list[middle]
 
-        const middle = Math.floor((low + high) / 2)
-        const guess = list[middle]
-
-        if (guess === item){
-            return {
-                time: new Date().getMilliseconds() - timer ,
-                index: middle,
-                times
-            }
+        if (guess === item) {
+            return middle
         }
+
         if (guess > item) {
             high = middle - 1
         } else {
             low = middle + 1
         }
     }
+
     return null
 }
 
